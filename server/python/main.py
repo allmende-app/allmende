b0fee0558dc6395583f00c6ccada586f9c462509
Minh Le <vuminhle@outlook.com>
2022-04-27 11:06:39 +0200

    Minh - docker done

--- server/python/main.py
+++ server/python/main.py
@@ -6,7 +6,7 @@ file = sys.argv[1]
 def predict(file: str):
     uploads = 'uploads'
     full_path = '{}/{}/{}'.format(os.getcwd(), uploads, file)
-    print(full_path)
+    print(full_path, end='')
     # sys.stdout.flush()
 
 predict(file)
