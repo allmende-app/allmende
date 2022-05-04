215c194a398fa53f72f23557d4bf67575d570fbc
Jonas <fischer-jon@t-online.de>
2022-05-04 14:30:21 +0200

    Jonas - setup VueJS

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style lang="sass">
+
+</style>
