6513dfa466294b7e69a259d5a48fd78accdde45e
Jonas <fischer-jon@t-online.de>
2022-05-09 14:29:54 +0200

    Jonas - initial style setup + components button and text input added

---
+++ client/src/components/VButton.vue
@@ -0,0 +1,77 @@
+<template>
+  <button :class="type" @click="sayHello" :disabled="disabled">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import type { PropType } from "@vue/runtime-core";
+
+export default defineComponent({
+  emits: ["click"],
+  props: {
+    type: {
+      type: String as PropType<string>,
+      default: "primary",
+      // INFO: use arrow function here, otherwise there is an waring when instanciating VButton!
+      validator: (value: PropType<string>) => ['primary', 'secondary'].includes(String(value))
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, { emit }) {
+    emit('click')
+
+    const sayHello = () => {
+      console.log("Hello");
+      emit('click')
+    }
+
+    return { sayHello }
+  }
+})
+
+</script>
+
+<style lang="sass" scoped>
+@import "../assets/sass/config.sass"
+
+$button-border-radius: 32px
+
+button
+  background-color: #fff
+  border: none
+  border-radius: $button-border-radius
+  padding: 16px 24px
+  border: 2px solid transparent
+  cursor: pointer
+  font-family: $main-font
+
+
+  &.primary
+    background-color: $primary
+    color: $font-inverted
+    &:hover
+      background-color: $primary-hover
+    &:disabled
+      background-color: $primary-disabled
+    &:active
+      background-color: $primary-active
+  &.secondary
+    background-color: $secondary
+    color: $font-primary
+    &:hover
+      background-color: $secondary-hover
+    &:disabled
+      background-color: $secondary-disabled
+    &:active
+      background-color: $secondary-active
+  &:focus
+      border-color: $border-color-focus
+
+
+
+</style>
---
+++ client/src/components/VInput.vue
@@ -0,0 +1,94 @@
+<template>
+  <div :class="'input-container ' + (error ? 'error' : '')">
+    <div class="input-wrapper">
+      <label :for="name">{{ label }}</label>
+      <input type="text" class="input-field" :name="name" :id="name" :value="value">
+    </div>
+    <div class="helper-text">
+      {{ helperText }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import type { PropType } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "VInput",
+  props: {
+    name: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    value: { // TODO als v-model binden!!!
+      type: String as PropType<string>,
+      default: ""
+    },
+    helperText: {
+      type: String as PropType<string>,
+      default: ""
+    },
+    label: {
+      type: String as PropType<string>,
+      default: ""
+    },
+    error: {
+      type: String as PropType<string>,
+      default: ""
+    }
+  },
+  setup(props) {
+    console.log("Input loaded");
+  }
+})
+
+</script>
+
+<style lang="sass" scoped>
+@import "../assets/sass/config.sass"
+
+// .frame-wrapper
+.input-container
+  .input-wrapper
+    label
+      position: absolute
+      left: 12px
+      margin-top: 4px
+      color: $font-secondary
+      font-size: $font-size-footnote
+
+    .input-field
+      background-color: $layer-color-10 // TODO set correct color here!!!
+      border-radius: $border-radius
+      border: none
+      font-family: $main-font
+      font-size: $font-size-body
+      padding: 12px
+      padding-top: 24px
+      padding-bottom: 10px
+
+      &:focus, &:active, &:focus-visible
+        outline: $border-color-focus solid $border-width
+
+
+  .helper-text
+    margin: 4px 12px
+    font-size: 10px
+    color: $font-secondary
+    font-size: $font-size-footnote
+
+  &.error
+    // background-color: red
+    .input-wrapper
+      label
+        color: $font-error
+
+      input
+        background-color: $layer-color-error
+
+    .helper-text
+      color: $font-error
+
+
+</style>
