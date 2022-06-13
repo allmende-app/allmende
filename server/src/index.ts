4d371a750b5c623b052c9bd8cbecbdb8c300732a
Minh Le <vuminhle@outlook.com>
2022-06-13 14:32:59 +0200

    Minh - fix error

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,8 @@
 import app from "./main";
 import { CONFIG, initializeFolders, connectDB, connectRedis } from "./config";
-import { avatarURLs, download_image } from "./utils";
 // import { Logger } from "./lib";
 
 app.listen(CONFIG.port, async () => {
-    const downloadUrls = avatarURLs.map((url, i) => download_image(url, `${process.cwd()}/uploads/random-${i}.jpg`));
-    await Promise.all(downloadUrls);
     try {
         const res = await connectDB();
         const redisClient = connectRedis();
@@ -14,7 +11,7 @@ app.listen(CONFIG.port, async () => {
             .then(() => console.log("Connected to Redis DB client"))
             .catch(console.error);
         // Logger.info("Connected to Redis DB client");
-        initializeFolders();
+        await initializeFolders();
         console.log(`Connected to DB: "${res.connections[0].name}"`);
         console.log(`Server listening on PORT: ${CONFIG.port}`);
     } catch (e) {
