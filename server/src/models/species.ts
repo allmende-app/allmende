ed41d5fcbaa884e7e5934cd0e3576c9e46b69df2
Linter Action <norepy@allmende>
2022-06-22 15:42:58 +0000

    Linter - Fix code style issues with Prettier for server

--- server/src/models/species.ts
+++ server/src/models/species.ts
@@ -40,92 +40,96 @@ export interface ISpeciesModel extends Model<ISpeciesDocument> {
     speciesExist: (id: string) => Promise<boolean>;
 }
 
-export const speciesSchema = new Schema<ISpeciesDocument>({
-    key: {
-        type: Schema.Types.Number, required: true,
+export const speciesSchema = new Schema<ISpeciesDocument>(
+    {
+        key: {
+            type: Schema.Types.Number,
+            required: true,
+        },
+        nubKey: {
+            type: Schema.Types.Number,
+        },
+        nameKey: {
+            type: Schema.Types.Number,
+        },
+        taxonID: {
+            type: Schema.Types.String,
+        },
+        sourceTaxonKey: {
+            type: Schema.Types.Number,
+        },
+        kingdom: {
+            type: Schema.Types.String,
+        },
+        kingdomKey: {
+            type: Schema.Types.Number,
+        },
+        phylumKey: {
+            type: Schema.Types.Number,
+        },
+        datasetKey: {
+            type: Schema.Types.String,
+        },
+        constituentKey: {
+            type: Schema.Types.String,
+        },
+        parentKey: {
+            type: Schema.Types.Number,
+        },
+        scientificName: {
+            type: Schema.Types.String,
+        },
+        canonicalName: {
+            type: Schema.Types.String,
+        },
+        vernacularName: {
+            type: Schema.Types.String,
+        },
+        authorShip: {
+            type: Schema.Types.String,
+        },
+        nameType: {
+            type: Schema.Types.String,
+        },
+        rank: {
+            type: Schema.Types.String,
+        },
+        origin: {
+            type: Schema.Types.String,
+        },
+        taxonomicStatus: {
+            type: Schema.Types.String,
+        },
+        nomenclaturalStatus: {
+            type: Schema.Types.Array,
+        },
+        remarks: {
+            type: Schema.Types.String,
+        },
+        numDescendants: {
+            type: Schema.Types.Number,
+        },
+        lastCrawled: {
+            type: Schema.Types.String,
+        },
+        lastInterpreted: {
+            type: Schema.Types.String,
+        },
+        issues: {
+            type: Schema.Types.Array,
+        },
+        synonym: {
+            type: Schema.Types.Boolean,
+        },
+        imageUrl: {
+            type: Schema.Types.String,
+        },
     },
-    nubKey: {
-        type: Schema.Types.Number,
+    {
+        timestamps: true,
+        validateBeforeSave: true,
     },
-    nameKey: {
-        type: Schema.Types.Number,
-    },
-    taxonID: {
-        type: Schema.Types.String,
-    },
-    sourceTaxonKey: {
-        type: Schema.Types.Number,
-    },
-    kingdom: {
-        type: Schema.Types.String,
-    },
-    kingdomKey: {
-        type: Schema.Types.Number,
-    },
-    phylumKey: {
-        type: Schema.Types.Number,
-    },
-    datasetKey: {
-        type: Schema.Types.String,
-    },
-    constituentKey: {
-        type: Schema.Types.String,
-    },
-    parentKey: {
-        type: Schema.Types.Number,
-    },
-    scientificName: {
-        type: Schema.Types.String,
-    },
-    canonicalName: {
-        type: Schema.Types.String,
-    },
-    vernacularName: {
-        type: Schema.Types.String,
-    },
-    authorShip: {
-        type: Schema.Types.String,
-    },
-    nameType: {
-        type: Schema.Types.String,
-    },
-    rank: {
-        type: Schema.Types.String,
-    },
-    origin: {
-        type: Schema.Types.String,
-    },
-    taxonomicStatus: {
-        type: Schema.Types.String,
-    },
-    nomenclaturalStatus: {
-        type: Schema.Types.Array,
-    },
-    remarks: {
-        type: Schema.Types.String,
-    },
-    numDescendants: {
-        type: Schema.Types.Number,
-    },
-    lastCrawled: {
-        type: Schema.Types.String,
-    },
-    lastInterpreted: {
-        type: Schema.Types.String,
-    },
-    issues: {
-        type: Schema.Types.Array,
-    },
-    synonym: {
-        type: Schema.Types.Boolean,
-    },
-    imageUrl: {
-        type: Schema.Types.String,
-    },
-}, {
-    timestamps: true,
-    validateBeforeSave: true
-});
+);
 
 speciesSchema.methods.construct = async function (data: ISpecies) {
     this.key = data.key || undefined;
@@ -157,17 +161,20 @@ speciesSchema.methods.construct = async function (data: ISpecies) {
     this.imageUrl = data.imageUrl || undefined;
 
     return this;
-}
+};
 
 speciesSchema.statics.findBySpeciesID = async function (id: string) {
     const doc = await this.findOne({ key: id });
     if (doc) return doc;
-}
+};
 
 speciesSchema.statics.speciesExist = async function (id: string) {
     const doc = await this.findOne({ key: id });
     if (doc) return true;
     else return false;
-}
+};
 
-export const Species = model<ISpeciesDocument, ISpeciesModel>("species", speciesSchema);
+export const Species = model<ISpeciesDocument, ISpeciesModel>(
+    "species",
+    speciesSchema,
+);
